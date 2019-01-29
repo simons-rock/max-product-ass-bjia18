877e68bffdc60fb8d641314a91b5aacf1b561843
bjia18 <bjia18@src-code.simons-rock.edu>
2019-01-29 00:14:14 -0500

    done

--- max_product_fast.c
+++ max_product_fast.c
@@ -1,6 +1,14 @@
 #include "max_product.h"
 
 int max_pairwise_product_fast(int *arr, int size){
-	return 0;
+	int a=0,b=1;
+	for (int i=0;i<size;i++){
+		if (arr[i]>arr[a]){
+			b=a;
+			a=i;
+		} else if ((i!=a)&&((arr[i]==arr[a] && i!=a) || (arr[i]>arr[b] && a!=b)))
+			b=i;
+	}
+	return arr[a]*arr[b];
 }
 
--- max_product_naive.c
+++ max_product_naive.c
@@ -1,5 +1,12 @@
 #include "max_product.h"
+#define max(x,y) (((x)>(y))?(x):(y))
 
 int max_pairwise_product_naive(int * arr, int size){
-	return 0;
+	int product=0;
+	for (int i=0;i<size;i++){
+		for (int j=i+1;j<size;j++){
+			product=max(product,arr[i]*arr[j]);
+		}
+	}
+	return product;
 }
